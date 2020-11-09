255a72f3f1ce98fab2a95b609354eedaab5e2620
Fahd Lyousfi <fahdlyousfi@gmail.com>
2020-11-09 11:25:48 +0100

    Finished Back

---
+++ src/main/java/com/example/beerFinder/BeerFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.beerFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beerFinder/BeerFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beerFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
