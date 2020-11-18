854a05197fb3aaf7c5bb4c02f0034d5dcd512e65
Fahd Lyousfi <fahdlyousfi@gmail.com>
2020-11-18 10:36:07 +0100

    replaced url with restTemplate

--- src/main/java/com/example/beerFinder/BeerController.java
+++ src/main/java/com/example/beerFinder/BeerController.java
@@ -1,55 +1,30 @@
 package com.example.beerFinder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 import org.json.JSONException;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class BeerController {
 
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{ \"data\" :");
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        sb.append((char) '}');
-
-        return sb.toString();
-    }
-
-    public static String readTextFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            return jsonText;
-        } finally {
-            is.close();
-        }
-    }
-
     @GetMapping("/beer")
     public ResponseEntity<String> greetingPost(@RequestParam(value = "name", defaultValue = "Punk") String name,
             @RequestParam(value = "page", defaultValue = "1") String page)
             throws URISyntaxException, IOException, JSONException {
-        String json = readTextFromUrl(
-                String.format("https://api.punkapi.com/v2/beers?beer_name=%s&page=%s", name.replace(" ", "_"), page));
-        return ResponseEntity.ok(json);
+
+        String GeneratedUrl = String.format("https://api.punkapi.com/v2/beers?beer_name=%s&page=%s",
+                name.replace(" ", "_"), page);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String res = restTemplate.getForObject(GeneratedUrl, String.class);
+        return ResponseEntity.ok("{ \"data\" :" + res + '}');
     }
 }
