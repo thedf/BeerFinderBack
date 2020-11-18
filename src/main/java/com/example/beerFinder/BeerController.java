3c49b28cd908b432688efdad923231284bbf13ab
Fahd Lyousfi <fahdlyousfi@gmail.com>
2020-11-18 10:17:43 +0100

    fix changes

--- src/main/java/com/example/beerFinder/BeerController.java
+++ src/main/java/com/example/beerFinder/BeerController.java
@@ -5,28 +5,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.http.HttpRequest;
 import java.nio.charset.Charset;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class BeerController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
     private static String readAll(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -40,26 +33,11 @@ public class BeerController {
         return sb.toString();
     }
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            // System.out.println(jsonText);
-
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
-        }
-    }
-
     public static String readTextFromUrl(String url) throws IOException, JSONException {
         InputStream is = new URL(url).openStream();
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String jsonText = readAll(rd);
-            // System.out.println(jsonText);
             return jsonText;
         } finally {
             is.close();
@@ -67,12 +45,7 @@ public class BeerController {
     }
 
     @GetMapping("/beer")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "You must be lost!";
-    }
-
-    @PostMapping("/beer")
-    public ResponseEntity<String> greetingPost(@RequestParam(value = "name", defaultValue = "Buzz") String name,
+    public ResponseEntity<String> greetingPost(@RequestParam(value = "name", defaultValue = "Punk") String name,
             @RequestParam(value = "page", defaultValue = "1") String page)
             throws URISyntaxException, IOException, JSONException {
         String json = readTextFromUrl(
